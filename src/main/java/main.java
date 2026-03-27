ad4197444355d26ec359e0e4f8eb8e0264a548d0
mythreyaans2023-cmyk <mythreyaan.s2023@vitstudent.ac.in>
2026-03-27 09:14:34 +0530

    Refactor main class to grades and add grading logic
    Refactored the main class to grades and added functionality to determine letter grades based on student marks.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,31 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello World by Mythreyaan S");
-    }
+package grade;
+import java.util.Scanner;
+
+public class grades {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the mark");
+		int student_mark=scanner.nextInt();
+		if(student_mark>=40) {
+			System.out.println("E");
+		}
+		else if(student_mark>=50) {
+			System.out.println("D");
+		}
+		else if(student_mark>=60) {
+			System.out.println("c");
+		}
+		else if(student_mark>=70) {
+			System.out.println("B");
+		}
+		else if(student_mark>=80) {
+			System.out.println("A");
+		}
+		else if(student_mark>=90) {
+			System.out.println("S");
+		}
+
+	}
+
 }
